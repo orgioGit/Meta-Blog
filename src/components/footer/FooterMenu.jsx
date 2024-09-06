ec224b9873cbf36083491eb9db25b529a21b0700
Бат-Оргил Даваасүрэн <24LP9786@G401-CC0101.local>
2024-09-06 10:03:38 +0800

    footer menu chenged

--- src/components/footer/FooterMenu.jsx
+++ src/components/footer/FooterMenu.jsx
@@ -2,19 +2,19 @@ export const FooterMenu = () => {
   return (
     <div className="w-full h-auto flex flex-col justify-start items-center gap-[8px]">
       <a
-        href=""
+        href="http://localhost:3000/"
         className="text-[16px] not-italic font-normal leading-6 text-[#3B3C4A] hover:text-[#D4A373]"
       >
         Home
       </a>
       <a
-        href=""
+        href="http://localhost:3000/blog-list"
         className="text-[16px] not-italic font-normal leading-6 text-[#3B3C4A] hover:text-[#D4A373]"
       >
-        About
+        Blog
       </a>
       <a
-        href=""
+        href="http://localhost:3000/contact-us"
         className="text-[16px] not-italic font-normal leading-6 text-[#3B3C4A] hover:text-[#D4A373]"
       >
         Contact
